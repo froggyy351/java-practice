b43859bb0aedb875f3a0620a39f746bd12f6a57d
sakoichi <kouichi19980817@gmail.com>
2026-01-07 23:49:01 +0900

    add Tiny

---
+++ TinyRPG.java
@@ -0,0 +1,12 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class TinyRPG {
+    public static void main (String args[]){
+        int playerHp = 100;
+        int enemyHp = 100;
+
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+    }
+}
