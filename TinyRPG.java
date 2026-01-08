c56ef6673f49d92b23b27b2097d74bbbf683e302
sakoichi <kouichi19980817@gmail.com>
2026-01-09 08:00:15 +0900

    update ty

--- TinyRPG.java
+++ TinyRPG.java
@@ -2,11 +2,30 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class TinyRPG {
-    public static void main (String args[]){
-        int playerHp = 100;
-        int enemyHp = 100;
 
-        Scanner scanner = new Scanner(System.in);
-        Random random = new Random();
+    static int playerHp = 100;
+    static int enemyHp = 100;
+    Scanner scanner = new Scanner(System.in);
+    Random random = new Random();
+
+    public static void main(String args[]){
+        while(true){
+            System.out.println("-- ターン開始 --");
+            System.out.println("あなた HP: " + playerHp);
+            System.out.println("敵 HP: " + enemyHp);
+        }
+    
     }
+
+    public void attack(){
+        enemyHp -= random.nextInt(11) + 10;
+    }
+
+    public void recover(){
+        playerHp += random.nextInt(6) + 5;
+        if (playerHp > 100){
+            playerHp = 100;   //回復は100を超えない。
+        }
+    }
+
 }
