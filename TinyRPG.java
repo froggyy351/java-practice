dd5312f8c7a71ba0239d1ef94d9c64645186e528
Koichi Sakogawa <kouichi19980817@gmail.com>
2026-01-10 23:56:56 +0900

    update TinyRPG.java

--- TinyRPG.java
+++ TinyRPG.java
@@ -3,29 +3,55 @@ import java.util.Scanner;
 
 public class TinyRPG {
 
-    static int playerHp = 100;
-    static int enemyHp = 100;
-    Scanner scanner = new Scanner(System.in);
-    Random random = new Random();
-
     public static void main(String args[]){
+
+        int playerHp = 100;
+        int enemyHp = 100;
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
         while(true){
-            System.out.println("-- ターン開始 --");
+            System.out.println("-- あなたのターン --");
             System.out.println("あなた HP: " + playerHp);
             System.out.println("敵 HP: " + enemyHp);
+            System.out.println("");
+            System.out.println("行動を選んでください");
+            System.out.println("1. 攻撃");
+            System.out.println("2. 回復");
+            System.out.println("3. 逃げる");
+            int command = scanner.nextInt();
+
+            switch (command) {
+                case 1:
+                    enemyHp -= random.nextInt(11) + 10;
+                    break;
+                case 2:
+                    playerHp += random.nextInt(6) + 5;
+                    if (playerHp > 100){
+                        playerHp = 100;   //回復は100を超えない。
+                    }
+                    break;
+                case 3:
+                    enemyHp -= random.nextInt(11) + 10;
+                    break;            
+                default:
+                    System.out.println("不正な値です。入力し直してください。");
+                    continue;
+            }
+
+            System.out.println("-- 敵のターン --");
+            System.out.println("敵は攻撃を繰り出した！");
+
+            playerHp -= random.nextInt(11) + 10;
+            System.out.println("-- 敵のターン --");
+
+            if (enemyHp <= 0) {
+                System.out.println("YOU WIN !!");
+            } else if (playerHp <= 0) {
+                System.out.println("YOU LOSE, GAME OVER...");
+            } else {
+                continue;
+            }
         }
-    
     }
-
-    public void attack(){
-        enemyHp -= random.nextInt(11) + 10;
-    }
-
-    public void recover(){
-        playerHp += random.nextInt(6) + 5;
-        if (playerHp > 100){
-            playerHp = 100;   //回復は100を超えない。
-        }
-    }
-
 }
