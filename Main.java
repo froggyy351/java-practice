2b55e3f85b65d251ec235e1213ac3721b1700fcd
sakoichi <kouichi19980817@gmail.com>
2026-01-24 21:38:11 +0900

    study about List

--- Main.java
+++ Main.java
@@ -1,16 +1,36 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World.");
 
         Person granpa = new Person();
         granpa.setAge(80);
-        granpa.setName("おじいちゃん");;
+        granpa.setName("おじい");;
         System.out.println(granpa);
 
         Person granma = new Person();
         granma.setAge(80);
-        granma.setName("おばあちゃん");;
+        granma.setName("おばあ");;
         System.out.println(granma);
         
+        System.out.println( granma.equals(granpa) );
+
+        granpa.setName("おばあ");
+        System.out.println( granma.equals(granpa) );
+
+        ArrayList<String> array = new ArrayList<>();
+
+        array.add("わし");
+        array.add("僕");
+        array.add("俺");
+        array.add("私");
+        array.add("それがし");
+        array.add("おいどん");
+
+        System.out.println("男の一人称だどん");
+        for(int i = 0; i < array.size(); i++){
+            System.out.println(array.get(i));
+        }
     }
 }
--- Person.java
+++ Person.java
@@ -6,6 +6,22 @@ public class Person {
         return "name:" + this.name + " / age:" + this.age;
     }
 
+    @Override
+    public boolean equals(Object obj) {
+        // 1. 同じメモリ番地（自分自身）なら当然 true
+        if (this == obj) { return true; }
+
+        // 2. 相手が Person クラスの仲間かどうかをチェック
+        if (obj instanceof Person) { 
+            Person p = (Person)obj;
+            // 3. 名前が同じなら true、違えば false
+            return this.name.equals(p.name);
+        }
+
+        // 4. 【重要】Personですらないなら、絶対に false！
+        return false; 
+    }
+
     //メンバ
     private int age;
     private String name;
